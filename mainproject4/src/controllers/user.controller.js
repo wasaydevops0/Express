53f4a7e205dec087382446683e505a43eb3af4e6
Abdul Wasay <abdulwasay0245@gmail.com>
2026-05-15 16:04:53 +0500

    built getCurrentUser controller

--- mainproject4/src/controllers/user.controller.js
+++ mainproject4/src/controllers/user.controller.js
@@ -215,9 +215,46 @@ const refreshAccessToken= asyncHandler(async (req,res) => {
 
 })
 
+const changeUserPassword = asyncHandler(async (req,res) => {
+    const { oldPassword, newPassword } = req.body;
+    const user = User.findById(req.user?._id);
+    const isPasswordCorrect = await user.
+        isPasswordCorrect(oldPassword);
+    if (!isPasswordCorrect) {
+        throw new ApiError(400,"Password is incorrect")
+    }
+
+    user.password = newPassword;
+    user.save({ validateBeforeSave: false });
+    
+    return res.
+        status(200).
+        json(
+            new ApiResponse(
+                200,
+                {},
+                "Password changed successfully"
+        )
+    )
+    
+
+})
+
+const getCurrentUser = asyncHandler(async (req,res) => {
+    return res
+        .status(200)
+        .json(
+            200,
+            req.user,
+            "Current user fetched successfully"
+    )
+})
+
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeUserPassword,
+    getCurrentUser
 }
