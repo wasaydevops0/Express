07d35bed5c404fdfd966aaea15276137d323a2f3
Abdul Wasay <abdulwasay0245@gmail.com>
2026-07-23 14:34:33 +0500

    made watch histroy controller

--- mainproject4/src/controllers/user.controller.js
+++ mainproject4/src/controllers/user.controller.js
@@ -341,8 +341,62 @@ const changeUserCoverImage = asyncHandler(async (req,res) => {
            )
     )
 })
+//TASK (DELETE OLD COVER AND AVATAR IMAGE)
 
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoos.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as:"watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1,
+                                        
+                                    }
+                                },
+
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                user[0].watchHistory
+        )
+    )
+})
 export {
     registerUser,
     loginUser,
@@ -352,4 +406,5 @@ export {
     getCurrentUser,
     changeUserDetails,
     changeUserAvatar,
+    changeUserCoverImage
 }
--- mainproject4/src/routes/user.routes.js
+++ mainproject4/src/routes/user.routes.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+    loginUser, 
+    logoutUser,
+    registerUser,
+    getWatchHistory,
+    changeUserAvatar,
+    changeUserCoverImage,
+    changeUserDetails,
+    changeUserPassword,
+    getCurrentUser,
+    refreshAccessToken,
+} from "../controllers/user.controller.js";
 import  {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { uploadCloudinary } from "../utils/cloudinary.js";
 
 const router = Router()
 
@@ -24,4 +36,28 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, 
+    changeUserPassword
+)
+router.route("/current-user").get(verifyJWT,
+    getCurrentUser
+)
+router.route("/update-account").patch(verifyJWT,
+    changeUserDetails
+)
+router.route("/change-avatar").patch(verifyJWT,
+    upload.single("avatar"), changeUserAvatar
+
+)
+
+router.route("/cover-image").patch(verifyJWT,
+    upload.single("coverImage"), changeUserCoverImage
+)
+router.route("/channel/:username").get(verifyJWT,
+    getCurrentUser
+)
+
+router.route("watchHistory").get(verifyJWT,
+    getWatchHistory
+)
 export default router
